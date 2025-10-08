4f943d14aba60baea5994e76762d3b6c1f93f1c2
Kévin Dunglas <kevin@dunglas.fr>
2025-10-08 11:52:40 +0200

    use d.Errf()

--- caddy.go
+++ caddy.go
@@ -134,7 +134,7 @@ func (g *Grpc) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 				}
 				g.MinThreads = t
 			default:
-				return fmt.Errorf(`unrecognized subdirective "%s"`, d.Val())
+				return d.Errf(`unrecognized subdirective "%s"`, d.Val())
 			}
 		}
 	}
