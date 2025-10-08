babbdb19312879acf453c3856ec18f425a698079
Kévin Dunglas <kevin@dunglas.fr>
2025-10-08 16:45:18 +0200

    simplify

--- grpc.go
+++ grpc.go
@@ -1,19 +1,13 @@
 package grpc
 
 import (
-	"net/http"
-	"net/url"
-
 	"github.com/dunglas/frankenphp"
 )
-
-var u = &url.URL{Host: "grpc.alt", Path: "/grpc"}
 
 func HandleRequest(request any) any {
 	responseChan := make(chan any)
 
 	w.InjectRequest(&frankenphp.WorkerRequest{
-		Request:            &http.Request{URL: u},
 		CallbackParameters: request,
 		AfterFunc: func(callbackReturn any) {
 			responseChan <- callbackReturn
